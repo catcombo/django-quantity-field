02a573c52c3fc2b50f97e760b4882de17c504988
Evgeniy Krysanov <evgeniy.krysanov@gmail.com>
2017-08-22 20:27:44 +0300

    Remove unnecessary tests

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -5,21 +5,11 @@ from django.test import TestCase
 from django.forms import modelform_factory
 
 from quantity_field import ureg
-from quantity_field.base import MultiQuantity
 from quantity_field.forms import MultiQuantityFormField
 from tests.models import Package
 
 
 class MultiQuantityFormTest(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        super(MultiQuantityFormTest, cls).setUpClass()
-        cls.TestForm = modelform_factory(Package, fields=("size",))
-
-        size = MultiQuantity.from_list(2, 5.5, 4, str(ureg.m))
-        weight = MultiQuantity.from_string('42 kilogram')
-        cls.entry = Package.objects.create(size=size, weight=weight)
-
     def test_form_field(self):
         self.assertRaises(ValidationError, MultiQuantityFormField)
         self.assertRaises(ValidationError, MultiQuantityFormField, dim=3.0)
@@ -30,10 +20,6 @@ class MultiQuantityFormTest(TestCase):
         self.assertRaises(ValidationError, MultiQuantityFormField, choices=[(ureg.g, 'gram'), (ureg.m, 'meter')])
 
     def test_form(self):
-        form = self.TestForm(data={'size_0': '2', 'size_1': '3.5', 'size_2': '11', 'size_3': 'meter'})
+        TestForm = modelform_factory(Package, fields=("size",))
+        form = TestForm(data={'size_0': '2', 'size_1': '3.5', 'size_2': '11', 'size_3': 'meter'})
         self.assertTrue(form.is_valid())
-
-    def test_model_form(self):
-        form = self.TestForm(data={'size_0': '2', 'size_1': '3.5', 'size_2': '11', 'size_3': 'meter'}, instance=self.entry)
-        self.assertTrue(form.is_valid())
-        form.save()
