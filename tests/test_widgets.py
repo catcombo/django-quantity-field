98be05fea93bb8873743fa77476d5592f37b1425
Evgeniy Krysanov <evgeniy.krysanov@gmail.com>
2017-08-22 20:29:08 +0300

    Add render-independent tests

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 
 from quantity_field.base import MultiQuantity
 from quantity_field.widgets import MultiQuantityWidget
+from .utils import contains_partial
 
 
 class MultiQuantityWidgetTest(TestCase):
@@ -11,8 +12,10 @@ class MultiQuantityWidgetTest(TestCase):
         mq = MultiQuantity.from_list(2, 'kg')
         widget = MultiQuantityWidget(dim=1, choices=[('gram', 'g'), ('kilogram', 'kg')])
 
-        self.assertIn('<input name="weight_0" type="text" />', widget.render('weight', None))
-        self.assertIn('<option value="kilogram">kg</option>', widget.render('weight', None))
+        self.assertTrue(contains_partial(widget.render('weight', None), '<input name="weight_0" type="text" />'))
+        self.assertTrue(contains_partial(widget.render('weight', None), '<option value="kilogram">kg</option>'))
 
-        self.assertIn('<input name="weight_0" type="text" value="2.0" />', widget.render('weight', mq))
-        self.assertIn('<option value="kilogram" selected="selected">kg</option>', widget.render('weight', mq))
+        self.assertTrue(contains_partial(widget.render('weight', mq),
+                                         '<input name="weight_0" type="text" value="2.0" />'))
+        self.assertTrue(contains_partial(widget.render('weight', mq),
+                                         '<option value="kilogram" selected="selected">kg</option>'))
